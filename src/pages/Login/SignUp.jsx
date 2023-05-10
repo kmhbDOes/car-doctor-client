28e0b72286741ad01cb37e8d02a36e9ece44e19b
kmhbDOes <kmhbipul@gmail.com>
2023-05-10 14:07:08 +0600

    firebase setup and auth provider setup

--- src/pages/Login/SignUp.jsx
+++ src/pages/Login/SignUp.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import img from "../../assets/images/login/login.svg";
 
-const Login = () => {
-  const handleLogin = (event) => {
+const SignUp = () => {
+  const handleSignUp = (event) => {
     event.preventDefault();
   };
   return (
@@ -13,8 +13,8 @@ const Login = () => {
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
-            <h1 className="text-3xl text-center font-bold">Login now!</h1>
-            <form onSubmit={handleLogin}>
+            <h1 className="text-3xl text-center font-bold">Sign Up!</h1>
+            <form onSubmit={handleSignUp}>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -23,6 +23,17 @@ const Login = () => {
                   type="text"
                   placeholder="email"
                   name="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="name"
+                  name="name"
                   className="input input-bordered"
                 />
               </div>
@@ -42,21 +53,29 @@ const Login = () => {
                   </a>
                 </label>
               </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Confirm Password</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                />
+              </div>
               <div className="form-control mt-6">
                 <input
                   className="btn btn-primary"
                   type="submit"
-                  value="Login"
+                  value="Sign Up"
                 />
               </div>
             </form>
             <p className="my-4 text-center">
-              New Here?
-              <Link
-                to="/sign-up"
-                className="link text-orange-600 font-bold ml-2"
-              >
-                Sign Up
+              Signed Up Already?
+              <Link to="/login" className="link text-orange-600 font-bold ml-2">
+                Log In
               </Link>
             </p>
           </div>
@@ -66,4 +85,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
