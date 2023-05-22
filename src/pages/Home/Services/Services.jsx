a27280b00d52aeb6520d19028992467aff42437c
kmhbDOes <kmhbipul@gmail.com>
2023-05-22 17:46:36 +0600

    searching added

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,14 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [ascending, setAscending] = useState(true);
+  const searchRef = useRef(null);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch("https://car-doctor-server-pi-three.vercel.app/services")
+    fetch(
+      `http://localhost:5000/services?search=${search}&sort=${
+        ascending ? "ascending" : "descending"
+      }`
+    )
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [ascending, search]);
+
+  const handleSearch = () => {
+    console.log(searchRef.current.value);
+    setSearch(searchRef.current.value);
+  };
   return (
     <div className="mt-4">
       <div className="text-center">
@@ -19,6 +31,38 @@ const Services = () => {
           humour, or randomized <br /> words which don't look even slightly
           believable.
         </p>
+        <div className="form-control">
+          <div className="input-group">
+            <input
+              type="text"
+              ref={searchRef}
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+            <button onClick={handleSearch} className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <button
+          className="btn btn-primary"
+          onClick={() => setAscending(!ascending)}
+        >
+          {ascending ? "Price: High to Low" : "Price: Low to High"}
+        </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {services.map((service) => (
